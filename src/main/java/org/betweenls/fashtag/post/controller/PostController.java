4ef1eadd2477abb1a5cb25bcd6c477c2d6222a7a
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-12-08 21:22:17 +0900

    hashtag test data 변경 / hashtag 클릭 시 Mapping 추가 / @Log->@Slf4j로 변경

--- src/main/java/org/betweenls/fashtag/post/controller/PostController.java
+++ src/main/java/org/betweenls/fashtag/post/controller/PostController.java
@@ -2,10 +2,12 @@ package org.betweenls.fashtag.post.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.betweenls.fashtag.post.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -13,7 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Controller
-@Log
+@Slf4j
 @AllArgsConstructor
 public class PostController {
     private PostService service;
@@ -23,8 +25,23 @@ public class PostController {
         model.addAttribute("list", service.getAllPost()); // 모든 포스트 가져오는 것
         // to-do 페이징 처리해서 requestparam에 따라 다른 정렬 순서 ㄱㄱ
         List<String> hashtagList = new ArrayList<>(Arrays.asList
-                ("#오뭐입", "#오오티디", "#김태희룩", "#얼죽코", "#패딩추천", "#겨울 핫트렌드","#꾸안꾸", "#나나누누"));
+                ("오뭐입", "OOTD", "겨울패션", "얼죽코", "패딩추천", "내뭐입","하이킹", "크리스마스"));
         model.addAttribute("hashtagList", hashtagList);
+
+        return "community/posts";
+    }
+
+    @GetMapping("/posts/tags/{hashtag}")
+    public String list(Model model,
+                       @RequestParam(defaultValue = "popular", required = false) String order,
+                       @PathVariable("hashtag") String hashtag) {
+
+        hashtag= "#" + hashtag;
+        model.addAttribute("list", service.getHashtagPost(hashtag));
+        List<String> hashtagList = new ArrayList<>(Arrays.asList
+                ("오뭐입", "OOTD", "겨울패션", "얼죽코", "패딩추천", "내뭐입","하이킹", "크리스마스"));
+        model.addAttribute("hashtagList", hashtagList);
+
         return "community/posts";
     }
 }
