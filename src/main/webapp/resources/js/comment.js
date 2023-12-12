cf4dfd99102af58e3885370701a71517078eec6b
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-13 03:01:25 +0900

    fix: comment.js 수정

--- src/main/webapp/resources/js/comment.js
+++ src/main/webapp/resources/js/comment.js
@@ -26,6 +26,7 @@ function getCommentList(){
                 <p class="created-date" data-formatted-date="${comment.formmatedCreatedAt}">${getTimeAgo(comment.formattedCreatedAt)}</p> </div>
                 <div class="delete-btn">삭제</div> </div>
                 `
+                //getTimeAgo 함수는 post-detail.js에 있음
             })
             const $comments = $(".comments-content");
             $comments.html(html);
@@ -37,37 +38,6 @@ function getCommentList(){
     })
 }
 
-
-
-//날짜를 뷰에 뿌릴 형식으로 변환하는 함수
-function getTimeAgo(date) {
-    const currentDate = new Date();
-    const postDate = new Date(date);
-    // const postLocalDate = new Date(postDate.getTime() + postDate.getTimezoneOffset() * 60 * 1000); //오라클시간 한국시간 아닐때 설정
-
-
-    const timeDifference = currentDate - postDate;
-    const seconds = Math.floor(timeDifference / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
-    const years = Math.floor(months / 12);
-
-    if (years > 0) {
-        return `${years}년 전`;
-    } else if (months > 0) {
-        return `${months}달 전`;
-    } else if (days > 0) {
-        return `${days}일 전`;
-    } else if (hours > 0) {
-        return `${hours}시간 전`;
-    } else if (minutes > 0) {
-        return `${minutes}분 전`;
-    } else {
-        return `${seconds}초 전`;
-    }
-}
 
 //댓글 삭제 모달창 띄우는 함수
 function deleteBtnAction() {
