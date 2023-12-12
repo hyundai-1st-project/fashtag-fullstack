299c5a2c8c01a26aaaf4bb9d1175e5b2324d1bb4
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-12-12 13:43:25 +0900

    Update PostController.java

--- src/main/java/org/betweenls/fashtag/post/controller/PostController.java
+++ src/main/java/org/betweenls/fashtag/post/controller/PostController.java
@@ -5,6 +5,9 @@ import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.betweenls.fashtag.post.domain.PostVO;
 import org.betweenls.fashtag.post.service.PostService;
+import org.betweenls.fashtag.user.domain.CustomUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,7 +72,7 @@ public class PostController {
         //로그인 안된 상태면
 //        return "user/login";
 
-
+        
         // 로그인 된 상태면
         return "community/new";
     }
