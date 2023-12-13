cddb6117e94fc3d59bb7b75d7737379c193fffd6
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-14 02:31:20 +0900

    feat: mapper에 getLikeStatus추가

--- src/main/java/org/betweenls/fashtag/post/mapper/LikeMapper.java
+++ src/main/java/org/betweenls/fashtag/post/mapper/LikeMapper.java
@@ -4,6 +4,8 @@ import org.betweenls.fashtag.post.domain.LikeVO;
 
 public interface LikeMapper {
 //	public List<CommentVO> getCommentListByPostId(Long postId);
+
+	public String getLikeStatus(LikeVO like);
 	public int insertLike(LikeVO like);
 
 	public int deleteLike(LikeVO like);
