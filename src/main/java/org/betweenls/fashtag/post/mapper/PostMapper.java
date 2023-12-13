b90c6472f4f873aeb417f5d9064b3f1b9c984af8
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-12-13 10:45:42 +0900

    게시물 작성 mapper 작성

--- src/main/java/org/betweenls/fashtag/post/mapper/PostMapper.java
+++ src/main/java/org/betweenls/fashtag/post/mapper/PostMapper.java
@@ -1,26 +1,25 @@
 package org.betweenls.fashtag.post.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.betweenls.fashtag.post.domain.PostVO;
 
 
 import java.util.List;
 
 public interface PostMapper {
-//	public List<PostVO> getPost();
 	public List<PostVO> getAllPost(String order);
 	public List<PostVO> getHashtagPost(String hashtag);
-
 	public List<String> getHashtagByPostId(Long postId);
 
-//	public List<PostVO> getListWithPaging(Criteria cri);
 
-//	public void insert(PostVO post);
-//
+	public void insertPost(PostVO postVO);
+	public Long getHashtagIdByHashtagName(String hashtagName);
+	public void insertHashtag(String hashtagName);
+	public void insertPost_hashtag(@Param("postId") Long postId, @Param("hashtagId") Long hashtagId);
 //	public void insertSelectKey(PostVO post);
-//
 //	public PostVO read(Long postId);
-//
 //	public int delete(long postId);
-//
 //	public int update(PostVO post);
+//	public List<PostVO> getListWithPaging(Criteria cri);
+// 	public List<PostVO> getPost();
 }
