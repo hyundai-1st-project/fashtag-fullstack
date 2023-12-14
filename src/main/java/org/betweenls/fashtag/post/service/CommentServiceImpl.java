89007c593032c1483e29784255fe68cb3adfb645
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-14 18:52:16 +0900

    feat: CommentServiceImpl/getCommentList에서 시작, 끝Page만드는 로직 추가

--- src/main/java/org/betweenls/fashtag/post/service/CommentServiceImpl.java
+++ src/main/java/org/betweenls/fashtag/post/service/CommentServiceImpl.java
@@ -17,9 +17,11 @@ public class CommentServiceImpl implements CommentService {
     private CommentMapper mapper;
 
     @Override
-    public List<CommentVO> getCommentList(Long postId) {
+    public List<CommentVO> getCommentList(Long postId, Long page, int pageNum) {
         log.info("get...." + postId);
-        List<CommentVO> commentList = mapper.getCommentListByPostId(postId);
+        Long startPage = (page-1)*pageNum + 1;
+        Long endPage = page*pageNum;
+        List<CommentVO> commentList = mapper.getCommentListByPostId(postId, startPage, endPage);
 
         //댓글 각각의 createdAt을 String변환한 후 CommentVO에 set
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
