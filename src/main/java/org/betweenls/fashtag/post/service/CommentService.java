d413391be2a249fd9dea680bd75f836f50b7393b
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-15 10:41:06 +0900

    Fashtag test (#41)
    * 댓글 detail 마무리
    
    * fix: 프로필 수정 접근제한

--- src/main/java/org/betweenls/fashtag/post/service/CommentService.java
+++ src/main/java/org/betweenls/fashtag/post/service/CommentService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface CommentService {
 	public List<CommentVO> getCommentList(Long postId, Long page, int pageNum);
+	Long getCommentNum(Long postId);
 	public int insertComment(CommentVO comment);
 
 	public int removeComment(Long commentId);
