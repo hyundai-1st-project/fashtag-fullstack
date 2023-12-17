52eae00d71ae6f9a061de1128fa57c5321b2875c
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-17 10:52:49 +0900

    fix: DetailMapper 불필요한 주석 제거

--- src/main/java/org/betweenls/fashtag/post/mapper/DetailMapper.java
+++ src/main/java/org/betweenls/fashtag/post/mapper/DetailMapper.java
@@ -5,19 +5,6 @@ import org.betweenls.fashtag.post.domain.PostVO;
 import java.util.List;
 
 public interface DetailMapper {
-//	public List<PostVO> getPost();
-//	public List<PostVO> getAllPost();
-
-//	public List<PostVO> getListWithPaging(Criteria cri);
-
-	//	public void insert(PostVO post);
-//
-//	public void insertSelectKey(PostVO post);
-//
 	public PostVO getPostDetailByPostId(Long postId);
 	public List<String> getHashtagByPostId(Long postId);
-
-//	public int delete(long postId);
-//
-//	public int update(PostVO post);
 }
