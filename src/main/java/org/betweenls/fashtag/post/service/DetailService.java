9439de14c6523f5239fbe6bfef521bec9907e727
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-17 10:53:50 +0900

    fix: DetailService 불필요한 주석 제거

--- src/main/java/org/betweenls/fashtag/post/service/DetailService.java
+++ src/main/java/org/betweenls/fashtag/post/service/DetailService.java
@@ -3,11 +3,5 @@ package org.betweenls.fashtag.post.service;
 import org.betweenls.fashtag.post.domain.PostVO;
 
 public interface DetailService {
-    //	public void register(PostVO board);
     public PostVO getPostDetail(Long postId);
-//	public boolean modify(PostVO board);
-//	public boolean remove(Long bno);
-//	public List<PostVO> getList();
-
-//	public List<PostVO> getAllPost();
 }
